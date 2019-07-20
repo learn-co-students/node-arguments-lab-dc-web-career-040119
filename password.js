aacb2e834b6afa7101a27eea01d6073b040da4bd
github username <github email address>
2019-07-20 03:26:52 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,16 @@
 // code solution here
+module.exports = generatePassword = (arg) => {
+    let password = ''
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < arg; i++) {
+        password += characters[Math.floor(Math.random() * characters.length)]
+    }
+
+    return password
+
+}
+
+console.log(generatePassword(process.argv[2]))
+
+process.exit(0)
